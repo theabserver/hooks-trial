e9e3e1ab262970da30ba1cd844e1de1de6607051
Shawn D'Souza <shawnrussel@gmail.com>
2022-08-12 07:16:23 +0530

    useRef Hook

--- src/component/APIdisplay.jsx
+++ src/component/APIdisplay.jsx
@@ -1,11 +1,13 @@
-import React, {useState, useEffect, createContext} from 'react'
-import {ContextDisplay}
-from './ContextDisplay'
+import React, {useState, useEffect, createContext, useRef} from 'react'
+import {ContextDisplay} from './ContextDisplay'
 
 const UserContext = createContext()
 export {UserContext}
 
 export const APIdisplay = () => {
+  const [inputValue,
+    setInputValue] = useState('')
+  const previousInputValue = useRef('')
 
   const [user,
     setUser] = useState({})
@@ -24,6 +26,11 @@ export const APIdisplay = () => {
     fetchData()
   }, [user]) // only re render for change to user
 
+  // In the useEffect, we are updating the useRef current value each time the inputValue is updated by entering text into the input field.
+  useEffect(() => {
+    previousInputValue.current = inputValue
+  }, [inputValue])
+
   const hasUser = () => Object
     .keys(user)
     .length > 0
@@ -31,6 +38,12 @@ export const APIdisplay = () => {
   return (
     <UserContext.Provider value={user}>
       {hasUser() && <ContextDisplay/>}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}/>
+      <h4>Current Value: {inputValue}</h4>
+      <h4>Previous Value: {previousInputValue.current}</h4>
     </UserContext.Provider>
   )
 }
